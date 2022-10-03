2529e6593de697cc31bbf3684845b2620c6d6448
MahanBY <mahan.madiwal@blueyonder.com>
2022-10-03 18:06:30 +0530

    Ad latest push

--- src/uimaps/AnamolyDetection_UI.java
+++ src/uimaps/AnamolyDetection_UI.java
@@ -16,9 +16,14 @@ public class AnamolyDetection_UI {
 	public static final By drillThrough=By.xpath("//*[contains(text(),'Drill through')]");
 	public static final By drillThroughUnderstand=By.xpath("//*[contains(text(),'Drill through')]");
 	public static final By table_Understand=By.xpath("//div[@class='visual visual-tableEx allow-deferred-rendering']");
-
 	
-	public static final  By btn_Understand = By.xpath("//*[contains(@aria-label,'DrillDownMeasure Understand')]/..");
+	
+	
+	
+	
+	
+	//AD
+	/*public static final  By btn_Understand = By.xpath("//*[contains(@aria-label,'DrillDownMeasure Understand')]/..");
 	public static final  By navBtn_DrillThrough = By.xpath("//button[@title='Drill through']");
 	public static final  By navBtn_Understand = By.xpath("//button[@title='Understand']");
 	public static final  By navBtn_Investigate = By.xpath("//button[@title='Investigate']");
@@ -28,14 +33,14 @@ public class AnamolyDetection_UI {
 	public static final  By table_UnderStand_ColumnCount = By.xpath("//div[@role='presentation']//div[@role='columnheader']");
 	public static final  By exploraryOutlierItemStores = By.xpath("//*[@class='y axis hideLinesOnAxis setFocusRing']//ancestor::div//*[@data-sub-selection-object-name=\"data-labels\"]");
 	public static final  By exploraryOutlierItemStoresText = By.xpath("//*[@data-sub-selection-display-name='Visual_Axis_Title']");
+	*/
+	
+	/*public static final  By Importance_forecastability_rowheader = By.xpath("//div[@class='rowHeaders']//ancestor::div[@class='pivotTableCellWrap cell-interactive tablixAlignCenter ']");
+	public static final  By Importance_forecastability_coloumnheader=By.xpath("(//div[@class='innerContainer']//ancestor::div[@class='columnHeaders'])[2]");*/
 	
 	
-	public static final  By Importance_forecastability_rowheader = By.xpath("//div[@class='rowHeaders']//ancestor::div[@class='pivotTableCellWrap cell-interactive tablixAlignCenter ']");
-	public static final  By Importance_forecastability_coloumnheader=By.xpath("(//div[@class='innerContainer']//ancestor::div[@class='columnHeaders'])[2]");
 	
-	
-	
-	public static final  By table_UnderStand_RowCount = By.xpath("(//div[@role='row'])");	
+	/*public static final  By table_UnderStand_RowCount = By.xpath("(//div[@role='row'])");	
 	public static final  By span_OutlierInvestigationReportForItem = By.xpath("//span[contains(text(),'Outlier investigation report for Item')]");
 	public static final  By span_AndStore = By.xpath("//span[contains(text(),'and Store')]");	
 	public static final  By span_FirstITEM = By.xpath("//*[contains(@aria-label,'First ITEM')]");
@@ -47,6 +52,7 @@ public class AnamolyDetection_UI {
 	public static final  By btn_BusniessImpact = By.xpath("(//div[contains(@class,'pivotTableCellNoWrap')])[4]/..");
 	
 	public static final  By chart_OutlierInvestigation  = By.xpath("((//span[contains(text(),'Trend and Residual')]//ancestor::*[@class='ng-star-inserted'])[4]//*[@class='lineChartSVG']//*[@role='listbox'])[1]");	
+*/
 
 	public static final By chart_OutlierInvestigation_option=By.xpath("((//*[@class='lineChartSVG']//*[@role='listbox'])[1]//*[@class='interactivity-line sub-selectable'])[2]");
 	public static final By chart_SeasonalityTrendResidual  = By.xpath("((//span[contains(text(),'Trend and Residual')]//ancestor::*[@class='ng-star-inserted'])[4]//*[@class='lineChartSVG']//*[@role='listbox'])[2]");	
@@ -86,6 +92,7 @@ public class AnamolyDetection_UI {
 	public static final By card_OutlierTransactions_OutlierValue=By.xpath("(((//div[@class='vcGroupBody themableBackgroundColor themableBorderColorSolid'])[5]/visual-container-group)[2]//*[@class='value'])[3]");
 	public static final By chart_HomePage=By.xpath("//*[@role='treeitem']");
 	
+
 	//understnd Outerline item store
 	public static final By understand_OutlierItemStores_MainText=By.xpath("((((//div[@class='vcGroupBody themableBackgroundColor themableBorderColorSolid'])))[7]/visual-container-group[3]//span[text()='Outlier Item Stores'])");
 	public static final By understand_OutlierItemStores_MainValue=By.xpath("((((//div[@class='vcGroupBody themableBackgroundColor themableBorderColorSolid'])))[7]/visual-container-group[3]//*[@class='value'])[1]");
@@ -124,20 +131,47 @@ public class AnamolyDetection_UI {
 	
 	
 
-	
-	
-	
-	
-	
-	
-	
-
-	
-
-	
-	
+	public static final By span_Department=By.xpath("(//*[@class='value'])[3]");
+	public static final By span_SubCategory=By.xpath("(//*[@class='value'])[4]");
+	public static final By span_Region=By.xpath("(//*[@class='value'])[5]");
+	public static final By span_Category=By.xpath("(//*[@class='value'])[6]");
+	public static final By span_ObervationsFor=By.xpath("(//*[@class='value']//*)[8]");
 
 
+	public static final  By exploraryOutlierItemStores = By.xpath("//*[@class='y axis hideLinesOnAxis setFocusRing']//ancestor::div//*[@data-sub-selection-object-name=\"data-labels\"]");
+	public static final  By exploraryOutlierItemStoresText = By.xpath("//*[@data-sub-selection-display-name='Visual_Axis_Title']");
+	
+	
+	public static final  By Importance_forecastability_rowheader = By.xpath("//div[@class='rowHeaders']//ancestor::div[@class='pivotTableCellWrap cell-interactive tablixAlignCenter ']");
+	public static final  By Importance_forecastability_coloumnheader=By.xpath("(//div[@class='innerContainer']//ancestor::div[@class='columnHeaders'])[2]");
+
+	public static final By dd_options=By.xpath("//input[@aria-label='Search']//ancestor::div[@class='slicer-dropdown-content']//div[@class='slicerBody']//div[@class='row']");
+	public static final By btn_Understand = By.xpath("//*[contains(@aria-label,'DrillDownMeasure Understand')]/..");
+	public static final By navBtn_DrillThrough = By.xpath("//button[@title='Drill through']");
+	public static final By navBtn_Understand = By.xpath("//button[@title='Understand']");
+	public static final By navBtn_Investigate = By.xpath("//button[@title='Investigate']");
+	public static final By navBtn_PricePromotionsPLC = By.xpath("//button[@title='Price, Promotions, PLC & Inventory']");	
+	public static final By title_Understand = By.xpath("//div[text()='Understand']//ancestor::div[@class='contentTable']");
+	public static final By title_Investigate = By.xpath("//div[text()='Investigate']//ancestor::div[@class='contentTable']");	
+	public static final By table_UnderStand_ColumnCount = By.xpath("//div[@role='presentation']//div[@role='columnheader']");
+	public static final By table_UnderStand_RowCount = By.xpath("(//div[@role='row'])");	
+	public static final By span_OutlierInvestigationReportForItem = By.xpath("//span[contains(text(),'Outlier investigation report for Item')]");
+	
+	public static final By span_AndStore = By.xpath("//span[contains(text(),'and Store')]");	
+	public static final By span_FirstITEM = By.xpath("//*[contains(@aria-label,'First ITEM')]");
+	public static final By span_FirstLOCATION = By.xpath("//*[contains(@aria-label,'First LOCATION')]");
+	public static final By span_InterPlaySalesPricePromo = By.xpath("//span[contains(text(),'Interplay between sales, price, promotions, PLC & inventory for Item')]");
+	public static final By span_ItemFromIndex = By.xpath("(//*[@class='value'])[8]//*");
+	public static final By span_LocationFromIndex = By.xpath("(//*[@class='value'])[7]//*");
+	
+	public static final By tableHeader_Investigate_option = By.xpath("(//div[@class='columnHeaders'])[2]//div[contains(@class,'pivotTableCellNoWrap cell-interactive')]");
+	public static final By table_OutlierInvestigation_option = By.xpath("((//div[@class='bodyCells'])[2]//div[contains(@class,'pivotTableCellWrap')])");
+	public static final By contextMenu_Option = By.xpath("//*[@role='menu']//button[@role='menuitem']");
+	public static final By contextMenu = By.xpath("//*[@role='menu']");	
+	public static final By btn_BusniessImpact = By.xpath("(//div[contains(@class,'pivotTableCellNoWrap')])[4]/..");	
+	public static final By chart_OutlierInvestigation  = By.xpath("((//span[contains(text(),'Trend and Residual')]//ancestor::*[@class='ng-star-inserted'])[4]//*[@class='lineChartSVG']//*[@role='listbox'])[1]");	
+  
+  
 	
 }
 
