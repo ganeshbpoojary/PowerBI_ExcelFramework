f2b66f6bf187f93d8614fb3822a4bab53ff72e64
Ganeshb-Poojary_ghub <Ganeshb.Poojary@blueyonder.com>
2022-10-13 12:41:52 +0530

    RPCC latest

--- src/testlogic/RPCC_Demand_Logic.java
+++ src/testlogic/RPCC_Demand_Logic.java
@@ -26,7 +26,7 @@ import com.thoughtworks.selenium.webdriven.commands.IsVisible;
 import frameworkutils.DataBook;
 import frameworkutils.WebDriverFactory;
 import uimaps.APPM_UI;
-import uimaps.AnamolyDetection_UI;
+import uimaps.RPCC_Demand_UI;
 import uimaps.RPCC_Demand_UI;
 import uimaps.Shopper_UI;
 import uimaps.Supplier_UI;
@@ -37,6 +37,8 @@ public class RPCC_Demand_Logic extends WebDriverFactory {
 		super(driver, dataBook);
 	}
 
+	
+	String filterName = dataBook.get("Fliter_Name");
 	/**
 	 * Validate Product Drill Through working
 	 * 
@@ -125,27 +127,53 @@ public class RPCC_Demand_Logic extends WebDriverFactory {
 	}
 
 	/**
-	 * Validate Needs Attention and Needs Review Button Validate Last Updated
-	 * message
+	 * Validate Needs Attention Button is active
 	 * 
 	 **/
-	public void Validate_NeedAttention_Review_RPCC_Demand() {
+	public void Validate_NeedAttentionIsActive_RPCC_Demand() {
 		synchronized (RPCC_Demand_Logic.class) {
 			boolean flag = false;
 			try {
-
-				isDisplayed(RPCC_Demand_UI.span_LastUpdated);
-				waitForElementTobeClickable(RPCC_Demand_UI.btn_NeedsAttention);
-				waitForElementTobeClickable(RPCC_Demand_UI.btn_NeedsReview);
-
+//				isDisplayed(RPCC_Demand_UI.span_LastUpdated);
+				waitForElementTobeClickable(RPCC_Demand_UI.btn_NeedsAttention);				
 				actionClick(RPCC_Demand_UI.btn_NeedsAttention);
 				waitForElementTobeLocated(RPCC_Demand_UI.table_AttentionReview);
 				isDisplayed(RPCC_Demand_UI.table_AttentionReview);
-
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	/**
+	 * Validate Needs Attention Button is active
+	 * 
+	 **/
+	public void Validate_LastUpdatedIsPresent_RPCC_Demand() {
+		synchronized (RPCC_Demand_Logic.class) {
+			boolean flag = false;
+			try {
+				isDisplayed(RPCC_Demand_UI.span_LastUpdated);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Validate Needs Review Button is active
+	 * 
+	 **/
+	public void Validate_NeedAReviewIsActive_RPCC_Demand() {
+		synchronized (RPCC_Demand_Logic.class) {
+			boolean flag = false;
+			try {
+				isDisplayed(RPCC_Demand_UI.span_LastUpdated);
+				waitForElementTobeClickable(RPCC_Demand_UI.btn_NeedsReview);
 				actionClick(RPCC_Demand_UI.btn_NeedsReview);
 				waitForElementTobeLocated(RPCC_Demand_UI.table_AttentionReview);
 				isDisplayed(RPCC_Demand_UI.table_AttentionReview);
-
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -153,10 +181,10 @@ public class RPCC_Demand_Logic extends WebDriverFactory {
 	}
 
 	/**
-	 * Validate Needs Attention & Needs Review Drill through
+	 * Validate Needs Attention Drill through
 	 * 
 	 **/
-	public void Validate_DrillThrough_RPCC_Demand() {
+	public void Validate_NeedsAttention_DrillThrough_RPCC_Demand() {
 		synchronized (RPCC_Demand_Logic.class) {
 			boolean flag = false;
 			try {
@@ -172,7 +200,21 @@ public class RPCC_Demand_Logic extends WebDriverFactory {
 				clickOn(RPCC_Demand_UI.navBtn_Understand);
 				waitForElementTobeLocated(RPCC_Demand_UI.title_Understand);
 				clickOn(RPCC_Demand_UI.link_Title);
-
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	/**
+	 * Validate Needs Review Drill through
+	 * 
+	 **/
+	public void Validate_NeedsReview_DrillThrough_RPCC_Demand() {
+		synchronized (RPCC_Demand_Logic.class) {
+			boolean flag = false;
+			try {
 				waitForElementTobeClickable(RPCC_Demand_UI.btn_NeedsReview);
 				actionClick(RPCC_Demand_UI.btn_NeedsReview);
 				waitForElementTobeLocated(RPCC_Demand_UI.table_AttentionReview);
@@ -317,7 +359,7 @@ public class RPCC_Demand_Logic extends WebDriverFactory {
 	 * 
 	 **/
 	public void validate_BarChartHasData_RPCC_Demand() {
-		synchronized (Shopper_Logic.class) {
+		synchronized (RPCC_Demand_Logic.class) {
 			try {
 				if (checkBarChartData())
 					extentTest.log(LogStatus.PASS, "Chart should have data","Chart - <B>"+"Forecast Trend"+"</B> is having data");
@@ -336,7 +378,7 @@ public class RPCC_Demand_Logic extends WebDriverFactory {
 	 * 
 	 **/
 	public void validate_TreeChartHasData_RPCC_Demand() {
-		synchronized (Shopper_Logic.class) {
+		synchronized (RPCC_Demand_Logic.class) {
 			try {
 				if (checkTreeChartData())
 					extentTest.log(LogStatus.PASS, "Chart should have data",
@@ -349,10 +391,204 @@ public class RPCC_Demand_Logic extends WebDriverFactory {
 			}
 		}
 	}
+	
+	/**
+	 * Validate Filter has Data, Multi Select, Search option
+	 * 
+	 **/
+	public void validate_FilterIsWorking_RPCC_Demand() {
+		synchronized (RPCC_Demand_Logic.class) {
+			try {
+				if (validate_DropDownIsWorking_RPCC_Demand())
+//					extentTest.log(LogStatus.PASS, "Chart should have data",							"Chart - <B>" +"Tree Map"+ "</B> is having data");
+				extentTest.log(LogStatus.PASS, "Drop Down Data presence validation", "Drop Down has Data");
+				else
+					extentTest.log(LogStatus.FAIL, "Drop Down Data presence validation", "Drop Down has no Data");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Navigates to Understand Page
+	 * 
+	 **/
+	public void goto_UnderstandPage_RPCC_Demand() {
+		synchronized (RPCC_Demand_Logic.class) {
+			try {
+				boolean flag = false;
+				int treeMapNum = getElementsCount(RPCC_Demand_UI.treeMap_HomePage_Options);
+				if(treeMapNum == -1)
+					flag = false;			
+				waitForElementTobeClickable(RPCC_Demand_UI.treeMap_HomePage_Options);
+				rightClick(RPCC_Demand_UI.treeMap_HomePage_Options);
+				waitForElementTobeClickable(RPCC_Demand_UI.navBtn_DrillThrough);
+				clickOn(RPCC_Demand_UI.navBtn_DrillThrough);
+				waitForElementTobeClickable(RPCC_Demand_UI.navBtn_Understand);
+				clickOn(RPCC_Demand_UI.navBtn_Understand);
+				waitForElementTobeLocated(RPCC_Demand_UI.title_Understand);
+				isDisplayed(RPCC_Demand_UI.title_Understand);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	public void validate_DropDownHasData_RPCC_Demand() {
+		synchronized (RPCC_Demand_Logic.class) {
+			try {
+				if (checkDropDownHasData()) {
+					extentTest.log(LogStatus.PASS, "Drop Down Data presence validation", "Drop Down has Data");
+				} else {
+					extentTest.log(LogStatus.FAIL, "Drop Down Data presence validation", "Drop Down has no Data");
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	public void goto_FilterOption_RPCC_Demand() {
+		synchronized (RPCC_Demand_Logic.class) {
+			try {
+				By filter = By.xpath("//*[@title='" + filterName + "']/../../../..//div[@role='combobox']");
+				waitForElementTobeClickable(filter);
+				clickOn(filter);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
 
+	public void validate_DropDownHasMultiSelect_RPCC_Demand() {
+		synchronized (RPCC_Demand_Logic.class) {
+			try {
+				if (isMultiSelectActive())
+					extentTest.log(LogStatus.PASS, "Drop down should have Multi Select",
+							"Multi Select option is working");
+				else
+					extentTest.log(LogStatus.FAIL, "Drop down should have Multi Select",
+							"Multi Select option is not working");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void validate_DropDownHasSearchOption_RPCC_Demand() {
+		synchronized (AnamolyDetection_Logic.class) {
+			try {
+				By searchOpt = By.xpath("//div[@role='listbox' and @aria-label='" + filterName + "']/..//input[@aria-label='Search']");
+				if (isDisplayed(searchOpt))
+					extentTest.log(LogStatus.PASS, "Search option should be present", "Search option is present");
+				 else 
+					 extentTest.log(LogStatus.FAIL, "Search option should be present", "Search option is not present");	
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/*************************************************************************************************************************************/
+	
 	/*
 	 * Private methods
 	 */
+	
+	private boolean checkDropDownHasData() {
+		try {
+			if (getFilterDropDownRowCount() > 0)
+				return true;
+			else
+				return false;
+		} catch (NoSuchElementException nsex) {
+			extentTest.log(LogStatus.FAIL, "Search Field vaildation", "Search filed is visible for filter");
+			return false;
+		}
+	}
+	
+	private int getFilterDropDownRowCount_Shooper() {
+		waitForElementTobeLocated(Shopper_UI.dd_options);
+		try {
+			List<WebElement> rows = driver.findElements(Shopper_UI.dd_options);
+			int size = rows.size();
+			extentTest.log(LogStatus.INFO, "getDropDownRowCount: ", "Number of rows is- <B>[" + size + "]</B>");
+			return size;
+		} catch (NoSuchElementException nsex) {
+			return -1;
+		}
+	}
+	
+	private boolean validate_DropDownIsWorking_RPCC_Demand() {
+		int rows = 0;
+		boolean flag = false;
+		try {
+			String[] filters = filterName.split("\\|");
+			List<String> itemList = Arrays.asList(filters);
+			for (int i = 0; i < itemList.size(); i++) {
+				String filter = itemList.get(i).trim();
+				By elm = By.xpath("//*[@title='" + filter + "']/../../../..//div[@role='combobox']");
+				waitForElementTobeClickable(elm);
+				clickOn(elm);
+//				waitForPageToLoad();
+				int index = i +1;
+				By optionWindow = By.xpath("(//div[@class='slicerBody'])["+index+"]");
+				waitForElementTobeLocated(optionWindow);
+				By options = By.xpath("(//div[@class='slicerBody'])["+index+"]//div[@class='row']");
+				rows = getElementsCount(options);
+				
+				//DropDown has data
+				if (rows > 0)
+					flag = true;
+				
+				//Drop Down has Search option
+				By search = By.xpath("//div[@aria-label='" + filter + "']/../..//input[@aria-label='Search']");
+				if (isDisplayed(search))
+					flag = true;
+				
+				//Drop had multiSelect Active
+				By selectAll = By.xpath("(//div[@class='slicerBody'])["+index+"]//*[text()='Select all']//ancestor::div[@class='slicerItemContainer']");
+				String state = getAttributeValueOf(selectAll, "aria-checked");
+//				String state = driver.findElement(RPCC_Demand_UI.dd_SelectAll_option).getAttribute("aria-checked");
+				if (state.trim().equals("true")) {
+					for (int j = 1; j <= rows; j++) {
+						String rowState = driver.findElement(By.xpath(
+								"(//input[@aria-label='Search']//ancestor::div[@class='slicer-dropdown-content']//div[@class='slicerBody']//div[@class='row'])["+j+"]//span[@class='slicerText']//ancestor::div[@class='slicerItemContainer']"))
+								.getAttribute("aria-checked");
+						if (rowState.trim().equals("true")) 
+							flag = true;
+						
+					}
+				} else if (state.trim().equals("false")) {
+					driver.findElement(RPCC_Demand_UI.dd_SelectAll_option).click();
+					String state2 = driver.findElement(RPCC_Demand_UI.dd_SelectAll_option).getAttribute("aria-checked");
+					if (state2.trim().equals("true")) {
+//						int rows = getFilterDropDownRowCount();
+						for (int j = 1; j <= rows; j++) {
+							String rowState = driver.findElement(By.xpath(
+									"(//input[@aria-label='Search']//ancestor::div[@class='slicer-dropdown-content']//div[@class='slicerBody']//div[@class='row'])["+j+"]//span[@class='slicerText']//ancestor::div[@class='slicerItemContainer']"))
+									.getAttribute("aria-checked");
+							if (rowState.trim().equals("true"))
+								flag = true;
+						}
+					}
+				}		
+				// Closing Drop down
+				clickOn(elm);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+	
 	private boolean checkBarChartData() {
 		try {
 			boolean flag = false;
@@ -387,15 +623,70 @@ public class RPCC_Demand_Logic extends WebDriverFactory {
 				actionClick(tree);
 				waitForPageToLoad();
 				snooze(3000);
-				String label = getTextOf(Shopper_UI.chart_popupText);
+				String label = getTextOf(RPCC_Demand_UI.chart_popupText);
 				if (!label.equals(""))
 					flag = true;
-				else
-					flag = false;
 			}
 			return flag;
 		} catch (NoSuchElementException e) {
 			return false;
 		}
+	}
+	
+	private int getFilterDropDownRowCount() {
+//		waitForElementTobeLocated(RPCC_Demand_UI.dd_options);
+		try {
+			List<WebElement> rows = driver.findElements(RPCC_Demand_UI.dd_options);
+			int size = rows.size();
+			extentTest.log(LogStatus.INFO, "getDropDownRowCount: ", "Number of rows is- <B>[" + size + "]</B>");
+			return size;
+		} catch (NoSuchElementException nsex) {
+			return -1;
+		}
+	}
+	
+	private boolean isMultiSelectActive() {
+		boolean flag;
+		try {
+			By selectAll = By.xpath("//div[@role='listbox' and @aria-label='" + filterName + "']//ancestor::div[@class='slicer-dropdown-content']//*[text()='Select all']//ancestor::div[@class='slicerItemContainer']");
+			String state = getAttributeValueOf(selectAll, "aria-checked");
+			if (state.trim().equals("true")) {			
+				By optCount = By.xpath("//div[@role='listbox' and @aria-label='" + filterName + "']//div[@class='row']");
+				int rows = getElementsCount(optCount);				
+				for (int i = 1; i <= rows; i++) {
+					String rowState = driver.findElement(By.xpath(
+							"(//input[@aria-label='Search']//ancestor::div[@class='slicer-dropdown-content']//div[@class='slicerBody']//div[@class='row'])["								+ i + "]//span[@class='slicerText']//ancestor::div[@class='slicerItemContainer']"))
+							.getAttribute("aria-checked");
+					if (rowState.trim().equals("true")) {
+						flag = true;
+					}
+				}
+				return true;
+			} else if (state.trim().equals("false")) {
+				clickOn(selectAll);
+				By selectAll2 = By.xpath("//div[@role='listbox' and @aria-label='" + filterName + "']//ancestor::div[@class='slicer-dropdown-content']//*[text()='Select all']//ancestor::div[@class='slicerItemContainer']");
+				String state2 = getAttributeValueOf(selectAll2, "aria-checked");
+				if (state2.trim().equals("true")) {
+					By optCount = By.xpath("//div[@role='listbox' and @aria-label='" + filterName + "']//div[@class='row']");
+					int rows = getElementsCount(optCount);
+					for (int i = 1; i <= rows; i++) {
+						String rowState = driver.findElement(By.xpath(
+								"(//input[@aria-label='Search']//ancestor::div[@class='slicer-dropdown-content']//div[@class='slicerBody']//div[@class='row'])["
+										+ i
+										+ "]//span[@class='slicerText']//ancestor::div[@class='slicerItemContainer']"))
+								.getAttribute("aria-checked");
+						if (rowState.trim().equals("true")) {
+							flag = true;
+						}
+					}
+					return true;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return false;
+
 	}
 }
